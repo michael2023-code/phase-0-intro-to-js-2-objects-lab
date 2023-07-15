e9cc13d9d6aa8fe610f76ec4ca14241f0ddcf4ca
michael2023-code <michael.mutunga@student.moringaschool.com>
2023-07-15 14:35:51 +0300

    objects assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {name:"Mike", streetAddress:"47-Judah"}
+function updateEmployeeWithKeyAndValue(employees, key, value){
+return Object.assign({}, employees, {[key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+employee[key] = value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+const NEW_OBJ = Object.assign({}, employee);
+delete NEW_OBJ[key]; 
+return NEW_OBJ;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+Object.assign({}, employee);
+delete employee[key];
+return employee;
+}
